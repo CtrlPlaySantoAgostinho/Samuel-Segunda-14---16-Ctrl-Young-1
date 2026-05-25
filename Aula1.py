935b41b446476e694cad46a814259c5b9917469c
Ctrl+Play Santo Agostinho <instrutores.bha@ctrlplay.online>
2026-05-25 14:59:05 -0300

    Create Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,37 @@
+def idade():
+    idade = int(input("Digite uma idade: "))
+
+    if idade < 18 and idade >= 0:
+        print("Menor de idade")
+
+    elif idade >= 18:
+        print("Maior de idade. A idade é: ", idade)
+
+    else:
+        print("Idade Invalida")
+
+#Operadores de Comparação
+
+# <
+# <=
+# >
+# >=
+# ==
+# !=
+
+#Operadores de Aritimeticos
+
+#Soma +
+#Subtração -
+#Multiplicação *
+#Divisão /
+#Elevado **
+#Divisão Inteira
+#Resto
+
+#And Todas as condições tem que ser verdadeiras
+#Or Somente uma condição tem que ser verdadeira
+
+#Estruturas condicionais
+
+#if
